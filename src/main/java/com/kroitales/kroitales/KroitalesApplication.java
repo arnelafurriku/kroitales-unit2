e09bf27fcf29dbda0148621a554e14baf0c472c0
Arnela Cuci <40432206+arnelafurriku@users.noreply.github.com>
2026-02-25 18:33:36 -0600

    Initial Spring Boot setup with MySQL configuration

---
+++ src/main/java/com/kroitales/kroitales/KroitalesApplication.java
@@ -0,0 +1,13 @@
+package com.kroitales.kroitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KroitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KroitalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kroitales/kroitales/KroitalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kroitales.kroitales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KroitalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
